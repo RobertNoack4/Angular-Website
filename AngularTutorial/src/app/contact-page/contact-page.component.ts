8413add37a78e5a1eda4c250087f3ac0a32f4eb7
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-01 13:31:32 +0200

    Commit All

--- AngularTutorial/src/app/contact-page/contact-page.component.ts
+++ AngularTutorial/src/app/contact-page/contact-page.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
-export class ContactPageComponent {
 
+export class ContactPageComponent {
+  email: string = '';
+  message: string = '';
+  listing: Listing;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id) as Listing;
+    this.message = `I am interested in your ${this.listing.name.toLowerCase()}!`;
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
+  }
 }
