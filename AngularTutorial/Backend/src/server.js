d405c42283e42742a584e556314d20484a64133e
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-01 16:01:26 +0200

    Added APi to get fake Listings

--- AngularTutorial/Backend/src/server.js
+++ AngularTutorial/Backend/src/server.js
@@ -1,19 +1,13 @@
 import Hapi from '@hapi/hapi';
+import routes from './routes';
 
 const start = async () => {
     const server = Hapi.server({
         port: 8000,
         host: 'localhost',
     });
-
-    server.route({
-        method: 'GET',
-        path: '/hello',
-        handler: (req, h) => {
-            return 'Hello!';
-        },
-    })
-
+    
+    routes.forEach(route => server.route(route));
     await server.start();
     console.log(`Server is listening on port ${server.info.uri}`)
 };
