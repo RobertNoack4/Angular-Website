1b045aa5f38b11dd9994fd843b65a2573c3e86b2
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-05 12:08:56 +0200

    Added createNewListings

--- AngularTutorial/Backend/src/routes/index.js
+++ AngularTutorial/Backend/src/routes/index.js
@@ -2,10 +2,12 @@ import { getAllListingsRoute } from "./getAllListingsRoute.js";
 import { getListingsRoute } from "./getListingRoute.js";
 import { addViewToListingRoute } from "./addViewToListing.js";
 import { getUserListingsRoute } from "./getUserListings.js";
+import { createNewListingsRoute} from "./createNewListings.js";
 
 export default [
     addViewToListingRoute,
     getAllListingsRoute,
+    createNewListingsRoute,
     getListingsRoute,
     getUserListingsRoute,
 ]
