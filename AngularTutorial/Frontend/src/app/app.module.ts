e20f15715639e4c91070510ed98aedf4f4866d1d
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-05 15:28:54 +0200

    Added Interface between Front- and Backend

--- AngularTutorial/Frontend/src/app/app.module.ts
+++ AngularTutorial/Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
