8f7a5e39cda97751e858d4aaa2a168ae9b62f27e
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-05 11:51:31 +0200

    Added getUserListings

--- AngularTutorial/Backend/src/routes/index.js
+++ AngularTutorial/Backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { getAllListingsRoute } from "./getAllListingsRoute.js";
 import { getListingsRoute } from "./getListingRoute.js";
 import { addViewToListingRoute } from "./addViewToListing.js";
+import { getUserListingsRoute } from "./getUserListings.js";
 
 export default [
     addViewToListingRoute,
     getAllListingsRoute,
     getListingsRoute,
+    getUserListingsRoute,
 ]
