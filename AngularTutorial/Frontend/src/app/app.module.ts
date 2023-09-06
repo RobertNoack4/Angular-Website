d99e8a76bdab7c1413b39cce9821ada557e81e65
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-06 12:54:38 +0200

    Speicherung vor Reinstall

--- AngularTutorial/Frontend/src/app/app.module.ts
+++ AngularTutorial/Frontend/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +13,9 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
-import { FormsModule } from '@angular/forms';
 import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
-import { environment } from 'src/environments/environment';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { environment } from '../environments/environment'; 
 
 @NgModule({
   declarations: [
@@ -35,7 +35,7 @@ import { environment } from 'src/environments/environment';
     FormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularTutorial/Frontend/src/environments/environment.development.ts
+++ AngularTutorial/Frontend/src/environments/environment.development.ts
@@ -1,1 +1,26 @@
-export const environment = {};
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyANVZ10PxX49jLzYArtbA7YaU_mTHBnrzI",
+      authDomain: "angular-tutorial-85d4f.firebaseapp.com",
+      projectId: "angular-tutorial-85d4f",
+      storageBucket: "angular-tutorial-85d4f.appspot.com",
+      messagingSenderId: "957713514255",
+      appId: "1:957713514255:web:4d647f5222496aec9b887a",
+      measurementId: "G-2Q1FN5JD2W"      
+    }
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  
