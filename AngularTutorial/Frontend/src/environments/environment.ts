3bace2352bee2e47e24f6f1e769f388edb19532b
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-06 10:43:08 +0200

    Added firebase Authentication to Front end

---
+++ AngularTutorial/Frontend/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyANVZ10PxX49jLzYArtbA7YaU_mTHBnrzI",
+        authDomain: "angular-tutorial-85d4f.firebaseapp.com",
+        projectId: "angular-tutorial-85d4f",
+        storageBucket: "angular-tutorial-85d4f.appspot.com",
+        messagingSenderId: "957713514255",
+        appId: "1:957713514255:web:4d647f5222496aec9b887a",
+        measurementId: "G-2Q1FN5JD2W"
+      }    
+};
